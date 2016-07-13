0443d1bebc9b27b54998cd2afa6961299f2fd60e
epiek <eve.pik@gmail.com>
2016-07-13 16:56:46 -0400

    improvements in group meeting

--- DataSorting_Resp.m
+++ DataSorting_Resp.m
@@ -1,4 +1,4 @@
-function [kdata_Under3,Traj_Under3,DensityComp_Under3,Res_Signal_Under]=DataSorting_Resp(kdata,Traj,DensityComp,Res_Signal,nline,para, Perc, Perr);
+function [kdata_Under3,Traj_Under3,DensityComp_Under3,Res_Signal_Under]=DataSorting_Resp(kdata,Traj,DensityComp,Res_Signal,nline,para, nResp, nCard);
 %Data sorting into two dynamic dimensions, one cardiac and one respiratory
 
 % [nx,ntviews,nc]=size(kdata);
@@ -42,39 +42,49 @@ for ii=1:para.CardiacPhase;
     DensityComp_Under1(:,:,:,ii)=DensityComp_Under1(:,:,index,ii);
 end
 
-tcar=floor(para.CardiacPhase/Perc);
-for ii=1:Perc
-    tmp1=kdata_Under1(:,:,:,:,(ii-1)*tcar+1:ii*tcar);
-    tmp2=Traj_Under1(:,:,:,(ii-1)*tcar+1:ii*tcar);
-    tmp3=DensityComp_Under1(:,:,:,(ii-1)*tcar+1:ii*tcar);
-    
-    tmp1=permute(tmp1,[1,2,5,3,4]);
-    tmp2=permute(tmp2,[1,2,4,3]);
-    tmp3=permute(tmp3,[1,2,4,3]);
-    
-    tmp1=reshape(tmp1,[nx,nline*tcar,nc,para.ntres]);
-    tmp2=reshape(tmp2,[nx,nline*tcar,para.ntres]);
-    tmp3=reshape(tmp3,[nx,nline*tcar,para.ntres]);
-    
-    kdata_Under2(:,:,:,:,ii)=tmp1;
-    Traj_Under2(:,:,:,ii)=tmp2;
-    DensityComp_Under2(:,:,:,ii)=tmp3;
-    
-end    
-
-tres=floor(para.ntres/Perr);
-for ii=1:Perr
-    tmp1=kdata_Under2(:,:,:,(ii-1)*tres+1:ii*tres,:);
-    tmp2=Traj_Under2(:,:,(ii-1)*tres+1:ii*tres,:);
-    tmp3=DensityComp_Under2(:,:,(ii-1)*tres+1:ii*tres,:);
-    
-    tmp1=permute(tmp1,[1,2,4,3,5]);
-    
-    tmp1=reshape(tmp1,[nx,nline*tcar*tres,nc,Perc]);
-    tmp2=reshape(tmp2,[nx,nline*tcar*tres,Perc]);
-    tmp3=reshape(tmp3,[nx,nline*tcar*tres,Perc]);
-    
-    kdata_Under3(:,:,:,ii,:)=tmp1;
-    Traj_Under3(:,:,ii,:)=tmp2;
-    DensityComp_Under3(:,:,ii,:)=tmp3;
+tres=floor(para.ntres/nResp);
+if (tres>1)
+    for ii=1:nResp
+        tmp1=kdata_Under1(:,:,:,:,(ii-1)*tres+1:ii*tres);
+        tmp2=Traj_Under1(:,:,:,(ii-1)*tres+1:ii*tres);
+        tmp3=DensityComp_Under1(:,:,:,(ii-1)*tres+1:ii*tres);
+        
+        tmp1=permute(tmp1,[1,2,5,3,4]);
+        tmp2=permute(tmp2,[1,2,4,3]);
+        tmp3=permute(tmp3,[1,2,4,3]);
+        
+        tmp1=reshape(tmp1,[nx,nline*tres,nc,para.ntres]);
+        tmp2=reshape(tmp2,[nx,nline*tres,para.ntres]);
+        tmp3=reshape(tmp3,[nx,nline*tres,para.ntres]);
+        
+        kdata_Under2(:,:,:,:,ii)=tmp1;
+        Traj_Under2(:,:,:,ii)=tmp2;
+        DensityComp_Under2(:,:,:,ii)=tmp3;
+    end
+else
+    disp('not enough spokes per image for this number of respiratory phases')
 end
+
+tcar=floor(para.ntres/nCard);
+if (tcar>1)
+    for ii=1:nCard
+        tmp1=kdata_Under2(:,:,:,(ii-1)*tcar+1:ii*tcar,:);
+        tmp2=Traj_Under2(:,:,(ii-1)*tcar+1:ii*tcar,:);
+        tmp3=DensityComp_Under2(:,:,(ii-1)*tcar+1:ii*tcar,:);
+        
+        tmp1=permute(tmp1,[1,2,4,3,5]);
+        
+        tmp1=reshape(tmp1,[nx,nline*tcar*tres,nc,nResp]);
+        tmp2=reshape(tmp2,[nx,nline*tcar*tres,nResp]);
+        tmp3=reshape(tmp3,[nx,nline*tcar*tres,nResp]);
+        
+        kdata_Under3(:,:,:,ii,:)=tmp1;
+        Traj_Under3(:,:,ii,:)=tmp2;
+        DensityComp_Under3(:,:,ii,:)=tmp3;
+    end
+else
+    disp('not enough spokes per image for this number of cardiac phases')
+end
+
+
+end%of function
