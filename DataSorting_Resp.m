6ea98a5cdb7b01bade88bfd2113bfce25db637d4
tchitiboi <teodora.chitiboi@nyumc.org>
2016-07-14 18:31:40 -0400

    set up custom dim size for resp and card

--- DataSorting_Resp.m
+++ DataSorting_Resp.m
@@ -25,20 +25,20 @@ for ii=1:para.CardiacPhase;
     DensityComp_Under1(:,:,:,ii)=DensityComp_Under1(:,:,index,ii);
 end
 
-tres=floor(para.ntres/nResp);
-if (tres>1)
-    for ii=1:nResp
-        tmp1=kdata_Under1(:,:,:,:,(ii-1)*tres+1:ii*tres);
-        tmp2=Traj_Under1(:,:,:,(ii-1)*tres+1:ii*tres);
-        tmp3=DensityComp_Under1(:,:,:,(ii-1)*tres+1:ii*tres);
+tcar=floor(para.CardiacPhase/nCard);
+if (tcar>0)
+    for ii=1:nCard
+        tmp1=kdata_Under1(:,:,:,:,(ii-1)*tcar+1:ii*tcar);
+        tmp2=Traj_Under1(:,:,:,(ii-1)*tcar+1:ii*tcar);
+        tmp3=DensityComp_Under1(:,:,:,(ii-1)*tcar+1:ii*tcar);
         
         tmp1=permute(tmp1,[1,2,5,3,4]);
         tmp2=permute(tmp2,[1,2,4,3]);
         tmp3=permute(tmp3,[1,2,4,3]);
         
-        tmp1=reshape(tmp1,[nx,nline*tres,nc,para.ntres]);
-        tmp2=reshape(tmp2,[nx,nline*tres,para.ntres]);
-        tmp3=reshape(tmp3,[nx,nline*tres,para.ntres]);
+        tmp1=reshape(tmp1,[nx,nline*tcar,nc,para.ntres]);
+        tmp2=reshape(tmp2,[nx,nline*tcar,para.ntres]);
+        tmp3=reshape(tmp3,[nx,nline*tcar,para.ntres]);
         
         kdata_Under2(:,:,:,:,ii)=tmp1;
         Traj_Under2(:,:,:,ii)=tmp2;
@@ -48,12 +48,12 @@ else
     disp('not enough spokes per image for this number of respiratory phases')
 end
 
-tcar=floor(para.ntres/nCard);
-if (tcar>1)
-    for ii=1:nCard
-        tmp1=kdata_Under2(:,:,:,(ii-1)*tcar+1:ii*tcar,:);
-        tmp2=Traj_Under2(:,:,(ii-1)*tcar+1:ii*tcar,:);
-        tmp3=DensityComp_Under2(:,:,(ii-1)*tcar+1:ii*tcar,:);
+tres=floor(para.ntres/nResp);
+if (tres>0)
+    for ii=1:nResp
+        tmp1=kdata_Under2(:,:,:,(ii-1)*tres+1:ii*tres,:);
+        tmp2=Traj_Under2(:,:,(ii-1)*tres+1:ii*tres,:);
+        tmp3=DensityComp_Under2(:,:,(ii-1)*tres+1:ii*tres,:);
         
         tmp1=permute(tmp1,[1,2,4,3,5]);
         
