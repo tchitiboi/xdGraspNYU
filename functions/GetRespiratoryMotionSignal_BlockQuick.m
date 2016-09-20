c3002067df8906f9bb45feb15792f4156e3d7112
tchitiboi <teodora.chitiboi@nyumc.org>
2016-09-20 15:33:30 -0400

    subtract center block from resp

--- functions/GetRespiratoryMotionSignal_BlockQuick.m
+++ functions/GetRespiratoryMotionSignal_BlockQuick.m
@@ -5,16 +5,32 @@ function [Res_Signal1,para]=GetRespiratoryMotionSignal_BlockQuick(para,maskHeart
 %if size(recon_Res,1) - size(mask,1) > 0
 %  mask = padarray(mask,[1, 1],0,'pre');
 %end
-se = strel('octagon',6);
-mask = imdilate(maskHeart,se);
+% se = strel('octagon',3);
+% mask = imdilate(maskHeart,se);
+
+recon_Res = medfilt1(abs(recon_Res),3,[],3);
 
 for t = 1:size(recon_Res,3)
   recon_Res(:,:,t) = imgaussfilt(abs(recon_Res(:,:,t)),2);
 end
-recon_Res = medfilt1(abs(recon_Res),3,[],3);
 
 [nx,ny,nt]=size(recon_Res);
-recon_Res = recon_Res .* repmat(imcomplement(mask),[1 1 size(recon_Res,3)]);
+%recon_Res = recon_Res .* repmat(imcomplement(maskHeart),[1 1 size(recon_Res,3)]);
+
+border_img = squeeze(recon_Res(:,:,1));
+border_size = 35;
+for x = 1:size(recon_Res,1)
+  for y = 1:size(recon_Res,2)
+      if (x < border_size || x > size(recon_Res,1)-border_size) ...
+         || (y < border_size || y > size(recon_Res,2)-border_size)
+          border_img(x,y) = 1;
+      else
+          border_img(x,y) = 0;  
+      end
+  end
+end
+
+recon_Res = recon_Res .* repmat(border_img,[1 1 size(recon_Res,3)]);
 
 TR=para.TR*2;
 time = TR:TR:nt*TR;
@@ -28,15 +44,15 @@ FC_Index=find(F_X<para.HF_H & F_X>para.LF_H);
 
 [nx,ny,nt]=size(recon_Res);
 %NN=floor(nx/16);k=0;
-NN=floor(nx/10);k=0;
+NN=floor(nx/16);k=0;
 for ii=1:3:nx-NN
     for jj=1:3:ny-NN
         %tmp=gpuArray(abs(recon_Res(jj:jj+NN-1,ii:ii+NN-1,:)));
         tmp=abs(squeeze(recon_Res(jj:jj+NN-1,ii:ii+NN-1,:)));
         bin_tmp = tmp;
-        bin_tmp(bin_tmp>0.0000001) = 1;
+        bin_tmp(bin_tmp>0.000000001) = 1;
         s = sum(sum(sum(bin_tmp,1),2),3);
-        if s/(NN*NN*nt) > 0.95
+        if s/(NN*NN*nt) > 0.7
             k=k+1;
             Signal(:,k)=squeeze(sum(sum(tmp,1),2));
             %Signal(:,k)= Signal(:,k)/(NN*NN);
@@ -53,9 +69,9 @@ Res_Peak=squeeze(Signal_FFT(FR_Index,:));
 Car_Peak=squeeze(Signal_FFT(FC_Index,:));
 
 ratio_Peak = max(Res_Peak)./max(Car_Peak);
-%[m,n]=find(Res_Peak==max(Res_Peak(:)));
-n = find(ratio_Peak == max(ratio_Peak));
-m = find(Res_Peak(:,n) == max(Res_Peak(:,n)));
+[m,n]=find(Res_Peak==max(Res_Peak(:)));
+%n = find(ratio_Peak == max(ratio_Peak));
+%m = find(Res_Peak(:,n) == max(Res_Peak(:,n)));
 
 disp(sprintf('Peak requency: %f', Res_Peak(m,n)));
 
@@ -67,7 +83,7 @@ para.ResFS=ResFS;
 
 Res_Signal=Signal(:,n);
 Res_Signal_FFT=Signal_FFT(:,n);
-Res_Signal=smooth(Res_Signal,5,'moving');
+Res_Signal=smooth(Res_Signal,7,'moving');
 
 %close all
 figure
@@ -76,9 +92,9 @@ subplot(2,1,2);plot(F_X,Res_Signal_FFT),set(gca,'XLim',[-1.5 1.5]),set(gca,'YLim
 figure,imagescn(abs(recon_Res),[0 .003],[],[],3)
 
 
-Res_Signal_Long = interp1( linspace(0,1,length(Res_Signal)), Res_Signal, linspace(0,1,nt*2), 'linear');
+Res_Signal_Long = interp1( linspace(0,1,length(Res_Signal)), Res_Signal, linspace(0,1,para.nt), 'linear');
 figure, plot(Res_Signal_Long)
-span = double(idivide(int32(para.span),2)*8+1);
+span = double(idivide(int32(para.span),2)*6+1);
 Res_Signal_Smooth = smooth(Res_Signal_Long, span, 'lowess');
 %figure, plot(Res_Signal_Smooth)
 [peak_values,peak_index]= findpeaks(double(Res_Signal_Smooth));
