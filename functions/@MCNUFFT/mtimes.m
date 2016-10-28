372d0cc8dd485111205ec44fbd8b156b34704dec
rramb <rebecca.ramb@nyumc.org>
2016-10-28 16:06:52 -0400

    normalization in operator

--- functions/@MCNUFFT/mtimes.m
+++ functions/@MCNUFFT/mtimes.m
@@ -8,10 +8,10 @@ if a.adjoint,
             b = bb(:,:,ch,tt).*a.w(:,:,tt);
             res(:,:,ch) = reshape(nufft_adj(b(:),a.st{tt})/sqrt(prod(a.imSize2)),a.imSize(1),a.imSize(2));
         end
-        ress(:,:,tt)=sum(res.*conj(a.b1),3)./sum(abs((squeeze(a.b1))).^2,3);
+        ress(:,:,tt)=sum(res.*conj(a.b1),3); %./sum(abs(squeeze(a.b1)).^2,3);
         clear res
     end
-    ress=ress.*size(a.w,1)*pi/2/size(a.w,2);
+    %ress=ress.*size(a.w,1)*pi/2/size(a.w,2);
 else
     % Cartesian image to multicoil non-Cartesian k-space
     for tt=1:size(bb,3),
