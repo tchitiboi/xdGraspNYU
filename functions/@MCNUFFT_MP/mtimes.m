68f511dfeea8635f38eb7695b87c92c141315bc5
rramb <rebecca.ramb@nyumc.org>
2016-10-28 16:12:50 -0400

    scaling in operator

--- functions/@MCNUFFT_MP/mtimes.m
+++ functions/@MCNUFFT_MP/mtimes.m
@@ -9,10 +9,11 @@ if a.adjoint,
                 b = bb(:,:,ch,resp,tt).*a.w(:,:,resp,tt);
                 res(:,:,ch) = reshape(nufft_adj(b(:),a.st{resp,tt})/sqrt(prod(a.imSize2)),a.imSize(1),a.imSize(2));
             end
-            ress(:,:,resp,tt)=sum(res.*conj(a.b1),3)./sum(abs((a.b1)).^2,3);clear res
+            ress(:,:,resp,tt)=sum(res.*conj(a.b1),3);%./sum(abs((a.b1)).^2,3);
+            clear res
         end
     end
-    ress=ress.*size(a.w,1)*pi/2/size(a.w,2);
+    %ress=ress.*size(a.w,1)*pi/2/size(a.w,2);
 else
     % Cartesian image to multicoil non-Cartesian k-space
     for tt=1:size(bb,4)
