f002fea4f7d9b6d35fcf08bdf0c0765fd15fe22c
Chitiboi <chitit01@xdglcdcdap002.nyumc.org>
2018-10-01 13:59:40 -0400

    fixed but with TR

--- functions/selectCardiacMotionFrequencies.m
+++ functions/selectCardiacMotionFrequencies.m
@@ -1,6 +1,6 @@
 function [FH_Index, F_X] = selectCardiacMotionFrequencies(para, nt)
 
-TR=para.TR*2;
+TR=para.TR*2; 
 time = TR:TR:nt*TR;
 F_S = 1/TR;F_X = 0:F_S/(nt-1):F_S;
 F_X=F_X-F_S/2;  %%% frequency after FFT of the motion signal
