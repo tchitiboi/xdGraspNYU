fd030150d979bc6f50b693af233630314172d1f5
Chitiboi <chitit01@xdglcdcdap002.nyumc.org>
2018-10-01 14:01:01 -0400

    fixed but with TR

--- functions/GetCardiacMotionSignal_HeartBlock.m
+++ functions/GetCardiacMotionSignal_HeartBlock.m
@@ -27,6 +27,7 @@ end
 [nx,ny,nt]=size(recon_Car);
 
 para.LF_H=1;para.HF_H=1.7;%%% initial heart rate range
+para.LF_H=0.8;para.HF_H=2;%%% initial heart rate range
 [HF_Index, F_X] = selectCardiacMotionFrequencies(para, nt);
 
 time_series = abs(recon_Car);
